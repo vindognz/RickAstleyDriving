7021a8b39dedf576474b5f3049434dbeb0fbdad3
Vincent <vindognz@gmail.com>
2024-11-09 09:38:42 +1300

    fixed some stuff idrk

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,14 @@
 extends RigidBody2D
 
 const SPEED = 5
-const t = 0.1
+const MAXt = 0.1
 
+var t = 0.1
 var lastRot = 0
 var lastDir = Vector2.UP
 var friction = 0.75
 
 func _process(delta: float) -> void:
-	if $RayCast2D.is_colliding():
-		print($RayCast2D.get_collider())
 	var direction := Input.get_vector("left", "right", "forward", "reverse")
 	
 	if Input.is_action_pressed("drift"):
@@ -30,7 +29,8 @@ func _process(delta: float) -> void:
 		
 	if lastRot - atan2(direction.y, direction.x) < -PI:
 		lastRot += 2*PI
-	
+
+	t = min(linear_velocity.length() / 500, 1) * MAXt
 	rotation = lastRot * (1-t) + atan2(direction.y, direction.x) * t
 	
 	lastRot = rotation
--- scripts/roadGenerator.gd
+++ scripts/roadGenerator.gd
@@ -6,8 +6,11 @@ var cornerTile = preload("res://scenes/cornerTile.tscn")
 var newestBranch
 var batchNum = 0
 var corner = 50
-var lastCornerRight = false
+var rightCornerCount = 0
 var parentList = []
+
+# npc cars, wider highways (sometimes), gas stations, police, surface stuff, bg music, sound effects, 
+# carpark that you spawn in facing the way you are forced to spawn in to make it act like im not bad at coding :)
 
 @onready var car: Node2D = $Car
 
@@ -18,7 +21,7 @@ func _process(delta: float) -> void:
 	if newestBranch != null:
 		if (newestBranch.global_position - car.transform.origin).length() < 1500:
 			batchNum += 1
-			genBatch(5)
+			genBatch(50)
 			
 			if batchNum > 5:
 				parentList.pop_back().queue_free()
@@ -34,15 +37,27 @@ func newTile(parent):
 			corner = 50
 			var tile = cornerTile.instantiate()
 			parent.add_child.call_deferred(tile)
-			if lastCornerRight:
-				tile.get_child(0).scale.y *= -1
-				tile.get_child(0).get_child(0).rotation = PI
-				tile.get_child(0).position.x = -24
+			print(rightCornerCount)
+			if rightCornerCount < 2 and rightCornerCount > -2:
+				if randi_range(0,2) == 0:
+					tile.get_child(0).scale.y *= -1
+					tile.get_child(0).get_child(0).rotation = PI
+					tile.get_child(0).position.x = -24
+					rightCornerCount -= 1
+				else:
+					rightCornerCount +=1
+			else:
+				if rightCornerCount == 2:
+					tile.get_child(0).scale.y *= -1
+					tile.get_child(0).get_child(0).rotation = PI
+					tile.get_child(0).position.x = -24
+					rightCornerCount -= 1
+				else:
+					rightCornerCount += 1
 				
 			tile.global_position = newestBranch.global_position
 			tile.rotation = newestBranch.global_rotation
 			newestBranch = tile.get_child(0).get_child(0)
-			lastCornerRight = not lastCornerRight
 		else:
 			corner -= 1
 			var tile = straightTile.instantiate()
@@ -52,7 +67,6 @@ func newTile(parent):
 			newestBranch = tile.get_child(0).get_child(0)
 
 func genBatch(length):
-	print("chur")
 	var parent = Node2D.new()
 	get_tree().root.add_child.call_deferred(parent)
 	parentList.push_front(parent)
